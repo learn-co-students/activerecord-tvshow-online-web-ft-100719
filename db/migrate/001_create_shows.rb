017b5ab23c1a4caf02abf7005728b76144ad8661
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-11-09 05:52:59 +0000

    Done.

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,17 @@
+class CreateShows < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.string :season
+      t.integer :rating
+    end
+  end
+
+
+end
+
+#
+#   
